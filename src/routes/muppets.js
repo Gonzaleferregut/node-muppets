fa2cf18098481b933a85fff14d712ab342cd2418
Rigoberto Gonzaleferregut <rigoberto.gonzaleferregut@gmail.com>
2017-10-31 11:57:39 -0500

    initial commit

---
+++ src/routes/muppets.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const muppetRouter = express.Router()
+const parseUrlencoded = bodyParser.urlencoded({ extended: false })
+
+let muppets = ['Kermit', 'Fozzy', 'Piggy', 'Beaker']
+
+muppetRouter.route('/')
+.get((req, res) => {
+  res.send(`Current muppets: ${muppets}`)
+})
+.post(parseUrlencoded, function (req, res) {
+  const newMuppet = req.body
+  console.log(newMuppet)
+  muppets.push(newMuppet.name)
+  console.log(muppets)
+  res.status(201).json(newMuppet.name)
+})
+
+muppetRouter.route('/:muppet')
+.delete((req, res) => {
+  console.log(req.params.muppet)
+  const index = muppets.indexOf(req.params.muppet)
+  if (index !== -1) {
+    muppets.splice(index, 1)
+    console.log(muppets)
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+export default muppetRouter
